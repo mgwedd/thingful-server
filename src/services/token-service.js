a86affdac775c7b3b56a7911d5086c948cc09a68
mgwedd <michael.wedd@leverege.com>
2019-08-01 23:06:42 -0400

    basic auth added

---
+++ src/services/token-service.js
@@ -0,0 +1,12 @@
+const config = require( '../config' )
+
+const TokenService = {
+  makeBasicAuthToken( userName, password ) {
+    return window.btoa( `${userName}:${password}` )
+  }, 
+  saveAuthToken( token ) {
+    window.localStorage.setItem( config.TOKEN_KEY, token )
+  }
+}
+
+export default TokenService
