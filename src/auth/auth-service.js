a5cb23b8215cebd32f3b05e7dc6e15e7cc0a8578
mgwedd <michael.wedd@leverege.com>
2019-08-03 19:55:56 -0400

    JWT implementation

--- src/auth/auth-service.js
+++ src/auth/auth-service.js
@@ -1,8 +1,15 @@
+const bcrypt = require( 'bcryptjs' )
+const jwt = require( 'jsonwebtoken' )
+const config = require( '../config' )
+
 const AuthService = {
   getUserWithUserName( db, user_name ) {
     return db( 'thingful_users' )
       .where( { user_name } )
       .first()
+  },
+  comparePasswords( password, hash ) {
+    return bcrypt.compare( password, hash )
   },
   parseBasicToken( token ) {
     return Buffer
@@ -10,6 +17,17 @@ const AuthService = {
       .toString()
       .split( ':' )
   },
+  createJwt( subject, payload ) {
+    return jwt.sign( payload, config.JWT_SECRET, {
+      subject,
+      algorithm : 'HS256',
+    } )
+  },
+  verifyJwt( token ) {
+    return jwt.verify( token, config.JWT_SECRET, {
+      algorithms : [ 'HS256' ],
+    } )
+  },
 }
-  
+
 module.exports = AuthService
